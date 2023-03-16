bf0c0d213785ba32d9954c7dedab1dc406be29f5
Shresth Sharma <101468675+Shresth-Sharma-2008@users.noreply.github.com>
2023-03-16 19:56:06 +0530

    Add files via upload

---
+++ js/top.js
@@ -0,0 +1,22 @@
+function home(){
+    window.open("/","_top");
+}
+function about(){
+    window.open("/html/about.html","_top")
+}
+function visit(){
+    window.open("https://goo.gl/maps/CWJGvLyCDeGeYw7w8","_top")
+}
+function gmail(){
+    navigator.clipboard.writeText("shree53111@gmail.com");
+    alert('g-mail copied: shree53111@gmail.com')
+}
+function whatsapp(){
+    window.open("https://api.whatsapp.com/send/?phone=919460353111&text&type=phone_number&app_absent=0","_top")
+}
+function review(){
+    window.open("","_top")
+}
+function feedback(){
+    window.open("/html/feedback.html","_top")
+}
